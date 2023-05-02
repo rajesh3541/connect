f6bd8798342ae51cf92e7644cd55da6df6d6f099
rajesh3541 <rajesh.buddi@accionlabs.com>
2023-05-02 13:01:42 +0530

    Updated xpaths of filters on accionites screen

--- tests/spec/accionites_spec.rb
+++ tests/spec/accionites_spec.rb
@@ -7,17 +7,17 @@ describe "On the Accioners page " do
         open_dashboard
         @driver.find_element(:xpath => "(//*[@id='hamburger-menu'])[1]").click
         @driver.find_element(:xpath => "//*[@id='drawer_container']/div/div/ul/div[1]/a/h5").click
-        sleep 2
+        sleep 10
         expect(@driver.find_element(:xpath => "//h1[contains(text(),'Accionites')]")).to be_truthy
     end
 
     it "verifies location filter functionality " do
         sleep 2
-        location = @driver.find_element(:xpath => "(//*[@id='autocomplete-search'])[1]")
+        location = @driver.find_element(:xpath => "//*[@id='autocomplete-location']")
         location.send_keys "Pune"
         location.send_keys:return
-        @driver.find_element(:xpath => "(//*[@id='autocomplete-search'])[1]").send_keys :down
-        @driver.find_element(:xpath => "(//*[@id='autocomplete-search'])[1]").send_keys :enter
+        @driver.find_element(:xpath => "//*[@id='autocomplete-location']").send_keys :down
+        @driver.find_element(:xpath => "//*[@id='autocomplete-location']").send_keys :enter
         sleep 5
         loc_list = @driver.find_elements(:xpath => "/html/body/div/main/div/div[2]/div/div/div/div[2]/div[3]/p/span")
         loc_list.each do |loc|
@@ -29,11 +29,11 @@ describe "On the Accioners page " do
 
     it "verifies role filter functionality " do
         sleep 2
-        role = @driver.find_element(:xpath => "(//*[@id='autocomplete-search'])[2]")
+        role = @driver.find_element(:xpath => "//*[@id='autocomplete-role']")
         role.send_keys "Team Work"
         role.send_keys:return
-        @driver.find_element(:xpath => "(//*[@id='autocomplete-search'])[2]").send_keys :down
-        @driver.find_element(:xpath => "(//*[@id='autocomplete-search'])[2]").send_keys :enter
+        @driver.find_element(:xpath => "//*[@id='autocomplete-role']").send_keys :down
+        @driver.find_element(:xpath => "//*[@id='autocomplete-role']").send_keys :enter
         sleep 5
         rol_list = @driver.find_elements(:xpath => "/html/body/div/main/div/div[2]/div/div/div/div[2]/div[2]/p/span")
         rol_list.each do |rol|
@@ -45,11 +45,11 @@ describe "On the Accioners page " do
 
     it "verifies skill filter functionality " do
         sleep 2
-        skill = @driver.find_element(:xpath => "(//*[@id='autocomplete-search'])[3]")
+        skill = @driver.find_element(:xpath => "//*[@id='autocomplete-skills']")
         skill.send_keys "Nexial"
         skill.send_keys:return
-        @driver.find_element(:xpath => "(//*[@id='autocomplete-search'])[3]").send_keys :down
-        @driver.find_element(:xpath => "(//*[@id='autocomplete-search'])[3]").send_keys :enter
+        @driver.find_element(:xpath => "//*[@id='autocomplete-skills']").send_keys :down
+        @driver.find_element(:xpath => "//*[@id='autocomplete-skills']").send_keys :enter
         sleep 5
         skl_list = @driver.find_elements(:xpath => "/html/body/div/main/div/div[2]/div/div/div/div[2]/div[5]/p/span")
         skl_list.each do |skl|
